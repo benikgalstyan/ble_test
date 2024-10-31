51accbe784a25124756ca84e9749993f8cba8dee
benik <galstyan.benik@gmail.com>
2024-10-31 18:28:55 +0200

    implemented main layout

--- lib/presentation/pages/main_screen/main_layout.dart
+++ lib/presentation/pages/main_screen/main_layout.dart
@@ -1,20 +1,43 @@
+import 'package:ble_test/core/context_extensions.dart';
+import 'package:ble_test/presentation/tokens/spacing.dart';
+import 'package:ble_test/presentation/widgets/app_wrapper.dart';
+import 'package:ble_test/presentation/widgets/state_specific_button.dart';
+import 'package:ble_test/presentation/widgets/status_switch.dart';
+import 'package:ble_test/presentation/widgets/status_text.dart';
+import 'package:ble_test/presentation/widgets/stop_button.dart';
+import 'package:ble_test/presentation/widgets/ble_info_display.dart';
 import 'package:flutter/material.dart';
 
-class MainLayout extends StatefulWidget {
-  const MainLayout({super.key});
+class MainLayout extends StatelessWidget {
+  const MainLayout({super.key, required this.bleName, required this.uuid});
+
+  final String bleName;
+  final String uuid;
 
   @override
-  State<MainLayout> createState() => _MainLayoutState();
-}
-
-class _MainLayoutState extends State<MainLayout> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Center(child: Text(context.s.restnowEmulator)),
+        ),
+        body: AppWrapper(
+          child: Center(
+            child: Center(
+              child: Column(
+                children: [
+                  BleInfoDisplay(uuid: uuid, bleName: bleName),
+                  Spacings.spacer64,
+                  const StatusText(),
+                  Spacings.spacer16,
+                  const StatusSwitch(),
+                  Spacings.spacer16,
+                  const StateSpecificButton(),
+                  Spacings.spacer16,
+                  const StopButton(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
 }
